e5555f446a93374e1071e1d1ec91dc297e112d38
Esteban <estebanmereles92@gmail.com>
2014-12-08 11:35:41 -0300

    Death script

---
+++ Assets/Scripts/Death.js
@@ -0,0 +1,11 @@
+﻿#pragma strict
+
+var player : GameObject;
+
+function OnTriggerEnter2D(hitCheck : Collider2D)
+{
+	if (hitCheck.name == "Ghost")
+	{
+		Application.LoadLevel (0);//Resets the scene, if static variables reasign them in Awake()
+	}	
+}
