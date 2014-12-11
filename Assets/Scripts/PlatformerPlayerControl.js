9cb45a6944b5d6792013abf57ab807b94eee8641
Esteban <estebanmereles92@gmail.com>
2014-12-11 16:35:27 -0300

    v0.2

--- Assets/Scripts/PlatformerPlayerControl.js
+++ Assets/Scripts/PlatformerPlayerControl.js
@@ -1,33 +1,44 @@
 #pragma strict
 
+var jumpType : String = "OverlapCircle"; //Default: OverlapCircle type of jump detection
+										//Optional: RayCasting
+
 //Horizontal movement variables
 var speed : float = 20;
 private var horizontalAxis : float;
 
 public var jumpForce : float;
 
-//Variables for ground collision check
+//Jump with RayCasting
 var groundLayer : LayerMask;
-var isGrounded : boolean = false;
+var raycastGround : RaycastHit2D;
+var raycastLenght : float = 0.5;
+
+//Jump with OverlapCircle
+//Coordinate of origin [(0,0) in reference to the player]
 var playerX : float;
 var playerY : float;
-var circleRadius : float = 1; 
+var circleRadius : float = 1;
+var isGrounded : boolean = false;
+
 
 //Variable for FlipSprite
 var scaleX : Vector3;
 
 function MovePlayer()
-{
-	playerX = transform.position.x;
-    playerY = transform.position.y;
+{	
 
 	if(Input.GetButtonDown("Jump"))//Unity has "Jump","Horizontal" and "Vertical" KeyDown built in
 	{
-		isGrounded = Physics2D.OverlapCircle(Vector2(playerX, playerY), circleRadius, groundLayer); //OverlapCircle(centerOfTheCircle, radius, layermask) [CircleRaycast]
-		if (isGrounded)
+		if (jumpType == "OverlapCircle")
 		{
-			rigidbody2D.AddForce(Vector2(0f, jumpForce),ForceMode2D.Impulse);
+			jmpWithOverlapCircle();
 		}
+		else if (jumpType == "RayCasting")
+		{
+			jmpWithRayCast();
+		}
+
 	}
 
 	else if (Input.GetAxis("Horizontal"))
@@ -35,6 +46,30 @@ function MovePlayer()
 		horizontalAxis = speed*Input.GetAxis("Horizontal"); //GetAxis returns 0.xxx or -0.xxx
 		rigidbody2D.velocity.x = horizontalAxis;
 		FlipSprite();
+	}
+
+}
+
+function jmpWithOverlapCircle()
+{
+	//Jump with OverlapCircle
+	playerX = transform.position.x;
+ 	playerY = transform.position.y;
+
+	isGrounded = Physics2D.OverlapCircle(Vector2(playerX, playerY), circleRadius, groundLayer); //OverlapCircle(centerOfTheCircle, radius, layermask) [CircleRaycast]
+	if (isGrounded)
+	{
+		rigidbody2D.AddForce(Vector2(0f, jumpForce),ForceMode2D.Impulse);
+	}
+}
+
+function jmpWithRayCast()
+{
+	//Jump with Raycasting
+	raycastGround = Physics2D.Raycast(transform.position, Vector3.down, raycastLenght, groundLayer);
+	if (raycastGround)
+	{
+		rigidbody2D.AddForce(Vector2(0f, jumpForce),ForceMode2D.Impulse);
 	}
 
 }
